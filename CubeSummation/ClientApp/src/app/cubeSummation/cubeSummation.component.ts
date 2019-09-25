c9931de0da4ccf71eed6fdcb066a8d86c33ba4a0
Ivan Dario Villota <idvillota@gmail.com>
2019-09-25 15:58:11 -0500

    Create Solution

---
+++ CubeSummation/ClientApp/src/app/cubeSummation/cubeSummation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-cubeSummation',
+    templateUrl: './cubeSummation.component.html'
+})
+export class CubeSummationComponent {
+
+}
